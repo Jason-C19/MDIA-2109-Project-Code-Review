e6b07e332af17a4c0b2d5e43eb2e0a3d2ef1608b
Jason <jchau38@my.bcit.ca>
2026-04-13 13:55:34 -0700

    Add interactive active states for navigation to JS

---
+++ script.js
@@ -0,0 +1,23 @@
+//Navigation://
+const navButtons = document.querySelectorAll(".nav__button");
+const categoryButtons = document.querySelectorAll(".category-nav__button");
+
+for (let i = 0; i < navButtons.length; i++) {
+  navButtons[i].addEventListener("click", function () {
+    for (let j = 0; j < navButtons.length; j++) {
+      navButtons[j].classList.remove("nav__button--active");
+    }
+
+    this.classList.add("nav__button--active");
+  });
+}
+
+for (let i = 0; i < categoryButtons.length; i++) {
+  categoryButtons[i].addEventListener("click", function () {
+    for (let j = 0; j < categoryButtons.length; j++) {
+      categoryButtons[j].classList.remove("category-nav__button--active");
+    }
+
+    this.classList.add("category-nav__button--active");
+  });
+}
