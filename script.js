3ea23053c21e6e59b682f7b63721ae6155a783e8
Shana <tluu18@my.bcit.ca>
2026-04-12 19:38:14 -0700

    feat: add product dataset for listing section

---
+++ script.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+        title: "Little Oak",
+        price: 40,
+        distance: "2.8km away",
+        recent: "2h ago",
+        image: "images/little-oak.png",
+    },
+    {
+        title: "ZZ Plant",
+        price: 5,
+        distance: "1.2km away",
+        recent: "1h ago",
+        image: "images/zz-plant.png",
+    },
+    {
+        title: "Coastline Sofa",
+        price: 70,
+        distance: "2km away",
+        recent: "3d ago",
+        image: "images/coastline-sofa.png",
+    },
+    {
+        title: "Timber Table",
+        price: 35,
+        distance: "5km away",
+        recent: "4d ago",
+        image: "images/timber-table.png",
+    },
+    {
+        title: "Oak Writing Desk",
+        price: 40,
+        distance: "5km away",
+        recent: "2h ago",
+        image: "images/oak-writing-desk.png",
+    },
+    {
+        title: "Round Dining Set",
+        price: 85,
+        distance: "6km away",
+        recent: "5d ago",
+        image: "images/round-dining-set.png",
+    },
+    {
+        title: "Tiny Plant",
+        price: 8,
+        distance: "1km away",
+        recent: "5m ago",
+        image: "images/tiny-plant.png",
+    },
+    {
+        title: "Natural Beech Chair",
+        price: 35,
+        distance: "5km away",
+        recent: "4d ago",
+        image: "images/natural-beech-chair.png",
+    },
+];
